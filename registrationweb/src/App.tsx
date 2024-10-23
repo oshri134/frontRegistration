02909ed6a404f7c431b347df8f82976b8768e592
oshri <54857364+oshri134@users.noreply.github.com>
2024-10-23 11:00:29 +0300

    Refactor App component to add ToastContainer for displaying notifications

--- registrationweb/src/App.tsx
+++ registrationweb/src/App.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import HomePage from "./pages/HomePage";
 import "./styles/global.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <HomePage />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </div>
   );
 };
